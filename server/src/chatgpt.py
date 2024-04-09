0b166f8ef23f9dc08e1d4468b2b3164bd23e66ae
Utkarsh Sharma <utkarshsharma@Utkarshs-MacBook-Air.local>
2024-04-09 14:46:19 -0500

    Fukll chat workflow in pythin, might have to switch to c# to be considtent with unity

--- server/src/chatgpt.py
+++ server/src/chatgpt.py
@@ -1,22 +1,66 @@
 import os
+import speech_recognition as sr
 from openai import OpenAI
 from dotenv import load_dotenv
+from playsound import playsound
 
 dotenv_path = './key.env'
 load_dotenv(dotenv_path=dotenv_path)
 
-client = OpenAI(
-    # This is the default and can be omitted
-    api_key=os.environ.get("OPENAI_API_KEY")
-)
+# Get the API key from the environment variables
+api_key = os.getenv("OPENAI_API_KEY")
 
-def generateSchedule(phrase):
-  completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-      {"role": "user", "content": "I will give you some words or phrases from the subject. Some words are abbreviations or abbreviations. I hope you will standardize this word or phrase. You don't need to reply with sentences, you just need to reply with the words or phrases you converted and nothing else. Here is the word: " + phrase + "."}
-    ]
-  )
+# Initialize OpenAI client with the API key
+client = OpenAI(api_key=api_key)
 
-  return completion.choices[0].message
+messages = [{"role": "system", "content":
+             "You are an intelligent assistant."}]
+
+r = sr.Recognizer()
+
+while True:
+    with sr.Microphone() as source:
+      print("Say something!")
+      audio = r.listen(source)
+    print ("Processing...")
+      #client = OpenAI()
+    with open("microphone-results.wav", "wb") as f:
+      f.write(audio.get_wav_data())
+
+    with open("microphone-results.wav", "rb") as f:
+      transcript = client.audio.transcriptions.create(
+      model="whisper-1", 
+      file=f,
+      response_format="text",
+      )
+      print(transcript)
+
+    message = transcript
+
+    if message: 
+        messages.append( 
+            {"role": "user", "content": message}, 
+        ) 
+        chat = client.chat.completions.create( 
+            model="gpt-3.5-turbo", messages=messages 
+        ) 
+    reply = chat.choices[0].message.content 
+    print(f"ChatGPT: {reply}") 
+    messages.append({"role": "assistant", "content": reply}) 
+
+    speech_filepath = "./speech.wav"
+    response = client.audio.speech.create(
+      model="tts-1",
+      voice="alloy",
+      input=reply
+    )
+
+    response.stream_to_file(speech_filepath)
+    # os.system("afplay " + speech_filepath)
+    playsound(speech_filepath)
+    print('playing sound using  playsound')
 #docker exec -it -d server-db-3 python3 -m jupyterlab --no-browser --ip=0.0.0.0 --port=5000 --allow-root --NotebookApp.token=''
+
+
+
+docker exec -it -d server-db-3 python3 -m jupyterlab --no-browser --ip=0.0.0.0 --port=5000 --allow-root --NotebookApp.token=''
